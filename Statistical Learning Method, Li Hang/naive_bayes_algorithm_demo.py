8f0e0b6e8e1f0ef6151d353fd09297834e38ae32
Louis Hsu <is.louishsu@foxmail.com>
2018-09-30 00:09:23 +0800

    补完朴素贝叶斯

--- Statistical Learning Method, Li Hang/naive_bayes_algorithm_demo.py
+++ Statistical Learning Method, Li Hang/naive_bayes_algorithm_demo.py
@@ -1,57 +1,100 @@
 import numpy as np
+from sklearn.preprocessing import OneHotEncoder
 
-X = [
-    [1, 'S'],
-    [1, 'M'],
-    [1, 'M'],
-    [1, 'S'],
-    [1, 'S'],
-    [2, 'S'],
-    [2, 'M'],
-    [2, 'M'],
-    [2, 'L'],
-    [2, 'L'],
-    [3, 'L'],
-    [3, 'M'],
-    [3, 'M'],
-    [3, 'L'],
-    [3, 'L']
-]
-y = [-1 ,-1, 1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1]
-X = np.array(X); y = np.array(y)
-X_test = np.array([2, 'S'])
 '''
             P(yc)ΠP(Xi|yc)
 P(yc|X) = ——————————————————
                 ΠP(Xi)
 '''
-X_cate = [['1', '2', '3'], ['S', 'M', 'L']]
-Y_cate = [-1, 1]
 
-p_yc = np.zeros(shape=(2,))
-for i in range(2):
-    p_yc[i] = y[y==Y_cate[i]].shape[0] / y.shape[0]
+class NaiveBayes():
+    def __init__(self):
+        self.featureEncoder = OneHotEncoder()
+        self.labelEncoder = OneHotEncoder()
+        self.n_labels = None; self.n_features = None
+        self.P_X = None                         # 各特征的概率              (n_features,)
+        self.P_Y = None                         # 各类别的概率              (n_labels,)
+        self.P_X_Y = None                       # 各类别下各特征的条件概率   (n_labels, n_features)
+    def fit(self, X, y):
+        X_encoded = self.featureEncoder.fit_transform(X).toarray()                  # toarray()将csr稀疏矩阵转换为稠密矩阵
+        y_encoded = self.labelEncoder.fit_transform(y.reshape((-1, 1))).toarray()   # toarray()将csr稀疏矩阵转换为稠密矩阵
+        self.P_X = np.mean(X_encoded, axis=0)                           # one-hot编码下，各列的均值即各特征的概率
+        self.P_Y = np.mean(y_encoded, axis=0)                           # one-hot编码下，各列的均值即各了别的概率
+        self.n_labels, self.n_features = y_encoded.shape[1], X_encoded.shape[1]   
+        self.P_X_Y = np.zeros(shape=(self.n_labels, self.n_features))   # 各个类别下，分别统计各特征的概率
+        for i in range(self.n_labels):
+            X_encoded_of_yi = X_encoded[y_encoded[:, i]==1]             # 取出属于i类别的样本
+            self.P_X_Y[i] = np.mean(X_encoded_of_yi, axis=0)            # one-hot编码下，各列的均值即各特征的概率
+        pass
+    def predict(self, X):
+        X_encoded = self.featureEncoder.transform(X).toarray()
+        n_samples = X_encoded.shape[0]
+        y_pred_prob = np.zeros(shape=(n_samples, self.n_labels))
+        for i in range(n_samples):
+            for j in range(self.n_labels):
+                P_Xi_encoded_Yj = X_encoded[i] * self.P_X_Y[j]          # 在Yj类别下，选出输入样本Xi对应的条件概率
+                P_Xi_encoded_Yj[P_Xi_encoded_Yj==0.0] = 1.0             # 将为0值替换为1，便于求解ΠP(Xi|yc)，只要将各元素累乘即可
+                P_Xi_encoded    = X_encoded[i] * self.P_X
+                P_Xi_encoded[P_Xi_encoded==0.0] = 1.0
+                y_pred_prob[i, j] = self.P_Y[j] * P_Xi_encoded_Yj.prod() / P_Xi_encoded.prod()
+                pass
+        return np.argmax(y_pred_prob, axis=1)
+    def score(self, y_true, y_pred):
+        ''' accuracy '''
+        return np.mean(np.equal(y_true, y_pred).astype('float'))
+        
+if __name__ == '__main__':
+    # X = [
+    #     [1, 'S'],
+    #     [1, 'M'],
+    #     [1, 'M'],
+    #     [1, 'S'],
+    #     [1, 'S'],
+    #     [2, 'S'],
+    #     [2, 'M'],
+    #     [2, 'M'],
+    #     [2, 'L'],
+    #     [2, 'L'],
+    #     [3, 'L'],
+    #     [3, 'M'],
+    #     [3, 'M'],
+    #     [3, 'L'],
+    #     [3, 'L']
+    # ]
+    # y = [-1 ,-1, 1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1]
+    
+    # 注意：
+    #   1. OneHotEncoder不接受字符类型的标称型数据
+    #   2. OneHotEncoder不接受小于0的标称型数据
+    X = [
+        [1, 0],
+        [1, 1],
+        [1, 1],
+        [1, 0],
+        [1, 0],
+        [2, 0],
+        [2, 1],
+        [2, 1],
+        [2, 2],
+        [2, 2],
+        [3, 2],
+        [3, 1],
+        [3, 2],
+        [3, 2],
+        [3, 2]
+    ]
+    y = [0 ,0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0]
+    X = np.array(X); y = np.array(y)
+    X_test = np.array([[2, 0], [1, 1]])
 
-p_Xi_yc = np.zeros(shape=(2, 2, 3))     # n_class x n_feature x n_value
-for i in range(2):
-    X_yi = X[y==Y_cate[i]]              # 筛选出y = yi的数据
-    for j in range(2):
-        X_yi_Xj = X_yi[:, j]
-        for k in range(3):
-            val = X_cate[j][k]
-            p_Xi_yc[i, j, k] = X_yi_Xj[X_yi_Xj==val].shape[0] / X_yi_Xj.shape[0]
+    estimator = NaiveBayes()
+    estimator.fit(X, y)
+    print('准确率: ', estimator.score(y, estimator.predict(X)))
 
-p_Xi = np.zeros(shape=(2, 3))          # n_feature x n_value
-for i in range(2):
-    Xi = X[:, i]
-    for j in range(3):
-        p_Xi[i, j] = Xi[Xi==X_cate[i][j]].shape[0] / Xi.shape[0]
+    y_pred = estimator.predict(X_test)
+    print(y_pred)
 
-p_yc_Xi = np.zeros(shape=(2,)) 
-for i in range(2):
-    p_yc_Xi[i] = p_yc[i]
-    for j in range(2):
-        p_yc_Xi[i] *= p_Xi_yc[i, j, X_cate[j].index(X_test[j])]
-
-print(Y_cate[np.argmax(p_yc_Xi)])
-            
+    '''
+    准确率:  0.7333333333333333
+    [0 0]
+    '''
