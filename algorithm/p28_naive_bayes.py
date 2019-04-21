e79198a0d1bd70693a0987bf7a864e28c7087674
louishsu <is.louishsu@foxmail.com>
2019-04-21 16:12:38 +0800

    log naive bayes

--- algorithm/p28_naive_bayes.py
+++ algorithm/p28_naive_bayes.py
@@ -102,6 +102,122 @@ class NaiveBayes():
         plt.scatter(n_samples, X[:, 3], c=y_pred)
         plt.show()
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+class NaiveBayesLog(object):
+    """ Naive Bayes estimator
+                   p(x|w_i)p(w_i)
+    p(w_i|x) = ----------------------
+                \sum_j p(x|w_j)p(w_j)
+    Attributes:
+        means_:         {ndarray(n_class, n_feats)}
+        covariances_:   {ndarray(n_class, n_feats, n_feats)}
+    """
+    def __init__(self):
+        
+        self.prior_ = None
+        self.means_ = None
+        self.covariances_ = None
+    
+    def fit(self, X, y):
+        """ train estimator
+        
+        Params:
+            X: {ndarray(n_samples, n_feats)}
+            y: {ndarray(n_samples)}          0, 1, 2, ...
+        """
+        n_class = len(set(list(y)))
+        n_feats = X.shape[1]
+
+        ## initialize
+        self.prior_ = np.zeros(shape=n_class)
+        self.means_ = np.zeros(shape=(n_class, n_feats))
+        self.covariances_ = np.zeros(shape=(n_class, n_feats, n_feats))
+
+        ## calculate mean and covariance of each class
+        for i_class in range(n_class):
+            idx = y==i_class
+            X_= X[idx]; y_ = y[idx]
+
+            means_ = np.mean(X_, axis=0)
+            X_ -= means_
+            covariances_ = X_.T.dot(X_) / (X_.shape[0] - 1)
+
+            self.prior_[i_class] = X_.shape[0] / X.shape[0]
+            self.means_[i_class] = means_
+            self.covariances_[i_class] = covariances_
+        
+    
+    def predict(self, X):
+        """ test estimator
+        
+        Params:
+            X:      {ndarray(n_samples, n_feats)}
+        Returns:
+            y_pred: {ndarray(n_samples)}
+        Notes:
+            In case of overflow: 
+                - use log(p(w_i|x)) instead.
+        """
+        
+        n_samples = X.shape[0]
+        n_class, n_feats = self.means_.shape
+
+        ## calculate probability of each sample
+        post_ = np.zeros(shape=(n_samples, n_class))
+        for i_class in range(n_class):
+
+            prior_ = self.prior_[i_class]
+            means_ = self.means_[i_class]
+            covariances_ = self.covariances_[i_class]
+            
+            for i_samples in range(n_samples):
+                ## log post probability
+                post_[i_samples, i_class] = self.logGaussian(X[i_samples], means_, covariances_) + np.log(prior_)
+
+        ## get predicted label
+        y_pred_proba = post_ - np.sum(post_, axis=0)
+        y_pred = np.argmax(y_pred_proba, axis=1)
+
+        return y_pred
+
+    @classmethod
+    def logGaussian(self, x, mean, covariances):
+        """ log multiple Gaussian distribution
+                            1                       1
+        p(x) = ----------------------------- exp [ --- (x - \mu)^T \Sigma^{-1} (x - \mu) ]
+                (2\pi)^{n/2} |\Sigma|^{1/2}         2
+                    1                                       n                1
+        log p(x) = --- (x - \mu)^T \Sigma^{-1} (x - \mu) - --- \log(2\pi) - --- \log(|\Sigma|) 
+                    2                                       2                2
+
+        Params:
+            x:      {ndarray(n_samples, n_feats)}
+            mean:   {ndarray(n_feats)}
+            covariance: {ndarray(n_feats, n_feats)}
+        Returns:
+            px:     {ndarray(n_samples)}
+        Notes:
+            In case of overflow: 
+                - use log(p(x)) instead.
+        """
+        n_feats = mean.shape[0]
+
+        x -= mean
+        covariances += np.eye(n_feats) * 1e-16
+        inv_covar = np.linalg.inv(covariances)
+        det_covar = np.linalg.det(covariances)
+
+        a = 0.5 * x.T.dot(inv_covar).dot(x)
+        b = (2*np.pi)**(n_feats/2) * np.sqrt(det_covar)
+
+        # px = np.exp(a) / b
+        logpx = a - np.log(b)
+        
+        return logpx
+
+
 if __name__ == "__main__":
     X, y = load_iris(return_X_y=True)
 
